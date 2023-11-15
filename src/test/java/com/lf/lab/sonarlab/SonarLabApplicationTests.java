83f0988f734f76fb00764ca4c151cb67ba98a74a
Bala K <bala@watermelon.us>
2023-11-15 09:53:43 +0530

    Base Code Base Code

---
+++ src/test/java/com/lf/lab/sonarlab/SonarLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lf.lab.sonarlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
