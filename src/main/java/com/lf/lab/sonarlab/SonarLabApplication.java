67294ca1d1abecfe78dcdb9da37971e1d45e266d
Bala K <bala@watermelon.us>
2023-11-16 10:23:26 +0530

    Base Code

--- src/main/java/com/lf/lab/sonarlab/SonarLabApplication.java
+++ src/main/java/com/lf/lab/sonarlab/SonarLabApplication.java
@@ -1,14 +1,9 @@
 package com.lf.lab.sonarlab;
 
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class SonarLabApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SonarLabApplication.class, args);
+		System.out.println("Testing");
 	}
 
 }
